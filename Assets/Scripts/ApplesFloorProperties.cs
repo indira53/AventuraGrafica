17d983632d80540c1dc99ae71fbf63a09077b7b4
Indira Guerrero Baez <indiraguebae@campus.monlau.com>
2024-11-03 20:29:27 +0100

    añadir el boton search en todo

--- Assets/Scripts/ApplesFloorProperties.cs
+++ Assets/Scripts/ApplesFloorProperties.cs
@@ -10,25 +10,45 @@ public class ApplesFloorProperties : MonoBehaviour, IPointerClickHandler
     private Inventory inventory;
     public GameObject gameManager;
     public GameObject Apple;
+    public GameObject player;
+
+    public bool meComiUnaManzana;
+
+    public float targetPositionX;
+    public float targetPositionY;   
+
 
     public string[] DialogueApplesFloorLook = { "Hay manzanas en el suelo." };
     public string[] DialogueApplesFloorTake = { "Tengo una manzana, pero necesito algo donde guardar las demás." };
-    public string[] DialogueApplesFloorUse = { "Sigo necesitango algo donde guardarlas." };
+    public string[] DialogueApplesFloorUse = { "Sigo necesitando algo donde guardarlas." };
+    public string[] DialogueApplesFloorSearch = { "Solo son manzanas." };
     public string[] DialogueApplesFloorUseWithCesta = { "¡Qué bien! Ahora tengo una cesta con manzanas." };
     public string[] DialogueApplesFloorUseWithApple = { "No puedo usar una manzana con las manzanas." };
     public string[] DialogueApplesFloorUseWithBranch = { "La rama no me sirve para guardar las manzanas." };
+    public string[] DialogueApplesFloorMeComiUnaManzana = { "No, ya me he comido una manzana, tengo que llevar las otras a casa." };
     public void OnPointerClick(PointerEventData eventData)
     {
         if (buttonsBehaviour.GetLookButton())
         {
             dialogueManager.Dialogue(DialogueApplesFloorLook);
+            player.GetComponent<Animator>().SetBool("isRunning", true);
         }
 
-        if (buttonsBehaviour.GetTakeButton())
+        if (buttonsBehaviour.GetTakeButton()) 
         {
-            dialogueManager.Dialogue(DialogueApplesFloorTake);
-            Apple.SetActive(false);
-            inventory.addItemsToInventory(Inventory.Items.Apple);
+            if (!meComiUnaManzana) 
+            {
+                dialogueManager.Dialogue(DialogueApplesFloorTake);
+                Apple.SetActive(false);
+                inventory.addItemsToInventory(Inventory.Items.Apple);
+
+                meComiUnaManzana = true;
+            }
+
+            else
+            {
+                dialogueManager.Dialogue(DialogueApplesFloorMeComiUnaManzana);
+            }
         }
 
         if (buttonsBehaviour.GetUseButton())
@@ -39,7 +59,6 @@ public class ApplesFloorProperties : MonoBehaviour, IPointerClickHandler
                 this.gameObject.SetActive(false);
                 inventory.addItemsToInventory(Inventory.Items.AppleCesta);
                 inventory.removeItemsFromInventory(Inventory.Items.Cesta);
-                
 
             }
 
@@ -60,8 +79,20 @@ public class ApplesFloorProperties : MonoBehaviour, IPointerClickHandler
                 dialogueManager.Dialogue(DialogueApplesFloorUse);
                 inventory.selectedItem = 0;
             }
+
+            else
+            {
+                dialogueManager.Dialogue(DialogueApplesFloorUse);
+            }
         }
+
+        if (buttonsBehaviour.GetSearchButton())
+        {
+            dialogueManager.Dialogue(DialogueApplesFloorSearch);
+        }
+
     }
+
 
 
     // Start is called before the first frame update
@@ -69,13 +100,16 @@ public class ApplesFloorProperties : MonoBehaviour, IPointerClickHandler
     {
         buttonsBehaviour = gameManager.GetComponent<ButtonsBehaviour>();
         dialogueManager = gameManager.GetComponent<DialogueManager>();
-        
+        targetPositionY = gameManager.transform.position.y;
         inventory = gameManager.GetComponent<Inventory>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (player.transform.position.x <= targetPositionX)
+        {
+            player.GetComponent<Animator>().SetBool("isRunning", false);
+        }
     }
 }
